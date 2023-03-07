0ffe2de9aab62b5935fb49b59448fa689b6cbc84
WhyHako <kolpascat@gmail.com>
2023-03-07 19:06:34 +0700

    first folder 'junior'

---
+++ Junior/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace SoftwareEngineeringNumber1
+{
+    public class Rootobject
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public Datum[] data { get; set; }
+        public Support support { get; set; }
+    }
+
+    public class Support
+    {
+        public string url { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Datum
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string avatar { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                var client = new RestClient("https://reqres.in");
+                var request = new RestRequest("api/users?page=2");
+                var response = client.Execute(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string rawResponse = response.Content;
+                    Rootobject result = JsonConvert.DeserializeObject<Rootobject>(rawResponse);
+
+                    for (int j = 0; j < result.data.Length; j++)
+                    {
+                        if (result.data[j].id == 10) Console.Write("id " + result.data[j].id + ": " + result.data[j].first_name + " " + result.data[j].last_name + " (" + result.data[j].email + ")");
+                    }
+                }
+            }
+        }
+    }
---
+++ Junior/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,173 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SoftwareEngineeringNumber2
+{
+    class Program
+    {
+        public class Rootobject
+        {
+            public Datum[] data { get; set; }
+        }
+
+        public class Datum
+        {
+            public string dept { get; set; }
+            public string name { get; set; }
+            public string phone { get; set; }
+            public int hours { get; set; }
+        }
+
+        public class JSON
+        {
+            public ОтделАСУ ОтделАСУ { get; set; }
+            public ОтделИнформационныхСистем Отделинформационныхсистем { get; set; }
+        }
+
+        public class ОтделАСУ
+        {
+            public int count { get; set; }
+            public int avg_hours { get; set; }
+            public Person[] people { get; set; }
+        }
+
+        public class Person
+        {
+            public string name { get; set; }
+            public string phone { get; set; }
+            public int hours { get; set; }
+        }
+
+        public class ОтделИнформационныхСистем
+        {
+            public int count { get; set; }
+            public int avg_hours { get; set; }
+            public Person[] people { get; set; }
+        }
+
+        public static void Main(string[] args)
+        {
+            JObject obj = JObject.Parse(@"{
+                          ""data"":[
+                  {
+                    ""dept"": ""Отдел информационных систем"",
+                    ""name"": ""Сотрудник 1"",
+                    ""phone"": ""89999999999""
+                  },
+                  {
+                    ""dept"": ""Отдел АСУ"",
+                    ""name"": ""Сотрудник 2"",
+                    ""phone"": ""88888888888""
+                  },
+                  {
+                    ""dept"": ""Отдел информационных систем"",
+                    ""name"": ""Сотрудник 3"",
+                    ""hours"": 165,
+                    ""phone"": ""87777777777""
+                  },
+                  {
+                    ""dept"": ""Отдел информационных систем"",
+                    ""name"": ""Сотрудник 4"",
+                    ""hours"": 132,
+                    ""phone"": ""86666666666""
+                  },
+                  {
+                    ""dept"": ""Отдел АСУ"",
+                    ""name"": ""Сотрудник 5"",
+                    ""hours"": 101,
+                    ""phone"": ""85555555555""
+                  },
+                  {
+                    ""dept"": ""Отдел информационных систем"",
+                    ""name"": ""Сотрудник 6"",
+                    ""hours"": 98,
+                    ""phone"": ""84444444444""
+                  }
+                ]}");
+
+            string json = obj.ToString();
+            Rootobject result = JsonConvert.DeserializeObject<Rootobject>(json);
+
+            var s = from f in result.data
+                    orderby f.dept
+                    select f;
+
+            int countDeptIS = 0;
+            int countDeptACS = 0;
+            int avghoursDeptACS = 0;
+            int avghoursDeptIS = 0;
+            List<int> listavghoursDeptACS = new List<int>();
+            List<int> listavghoursDeptIS = new List<int>();
+
+            foreach (var f in s)
+            {
+                if (f.dept == "Отдел информационных систем")
+                {
+                    countDeptIS++;
+                    if (f.hours != 0) listavghoursDeptIS.Add(f.hours);
+                }
+                if (f.dept == "Отдел АСУ")
+                {
+                    countDeptACS++;
+                    if (f.hours != 0) listavghoursDeptACS.Add(f.hours);
+                }
+            }
+
+            double avgdeptACS = listavghoursDeptACS.Average();
+            double avgdeptIS = listavghoursDeptIS.Average();
+
+            List<Person> persondeptIS = new List<Person>();
+            List<Person> persondeptACS = new List<Person>();
+            foreach (var f in s)
+            {
+                if (f.dept == "Отдел информационных систем")
+                    persondeptIS.Add(new Person { name = f.name, phone = f.phone, hours = f.hours });
+                if (f.dept == "Отдел АСУ")
+                    persondeptACS.Add(new Person { name = f.name, phone = f.phone, hours = f.hours });
+            }
+
+            JSON Json = new JSON();
+
+            ОтделАСУ deptACS = new ОтделАСУ();
+            ОтделИнформационныхСистем deptIS = new ОтделИнформационныхСистем();
+
+            deptACS.avg_hours = Convert.ToInt32(avgdeptACS);
+            deptIS.avg_hours = Convert.ToInt32(avgdeptIS);
+
+            deptACS.count = countDeptACS;
+            deptIS.count = countDeptIS;
+
+            deptACS.people = persondeptACS.ToArray();
+            deptIS.people = persondeptIS.ToArray();
+
+            Json.ОтделАСУ = deptACS;
+            Json.Отделинформационныхсистем = deptIS;
+
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamWriter sw = new StreamWriter(@"c:\users\lopas\desktop\final_json.txt"))
+            using (JsonWriter jw = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(sw, Json);
+            }
+
+            string StringJson = null;
+            JSON fresult;
+            using (StreamReader file = File.OpenText(@"c:\users\lopas\desktop\final_json.txt"))
+            {
+                fresult = (JSON)serializer.Deserialize(file, typeof(JSON));
+            }
+
+            Console.WriteLine("Отдел АСУ");
+            foreach (Person person in fresult.ОтделАСУ.people)
+            {
+                Console.WriteLine(person.name + " " + person.phone + " " + person.hours);
+            }
+
+            Console.WriteLine("Отдел ИС");
+            foreach (Person person in fresult.Отделинформационныхсистем.people)
+            {
+                Console.WriteLine(person.name + " " + person.phone + " " + person.hours);
+            }
+        }
+    }
+}
